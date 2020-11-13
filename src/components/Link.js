523f410db58eafca8af0a0b7ef2ac862ec62b3a5
Thethawat Kongyu <thethawat.k@mail.kmutt.ac.th>
2020-11-13 19:34:27 +0700

    [Add] Vanilla Routes

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,8 +2,14 @@ import React from "react";
 
 function Link({ className, href, children }) {
   const onClick = (e) => {
+    if (e.metaKey || e.ctrlKey) {
+      return;
+    }
     e.preventDefault();
     window.history.pushState({}, "", href);
+
+    const navEvent = new PopStateEvent("popstate");
+    window.dispatchEvent(navEvent);
   };
   return (
     <a href={href} className={className} onClick={(e) => onClick(e)}>
